3102182d32f74f6188d388a519ed070d6c2f29cf
Landon Shumway <landonrs@users.noreply.github.com>
2017-08-19 23:58:36 -0600

    Delete rectangle.py

--- rectangle.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-
-class Rectangle(pygame.sprite.Sprite):
-
-    def __init__(self):
-        super(Rectangle, self).__init__()
-        self.image = pygame.Surface([16, 16])
-        self.rect = pygame.Rect(0, 0, 16, 16)
